2bda83610dfea7a01468788ab716ca9071a54a0f
simple2126 <kimcw212@gmail.com>
2025-03-31 17:47:43 +0900

    [Update] OptionPanel
    볼륨 동기화 완료

--- Assets/Scripts/UI/OptionPanel.cs
+++ Assets/Scripts/UI/OptionPanel.cs
@@ -4,19 +4,26 @@ using UnityEngine.UI;
 
 public class OptionPanel : UIBase
 {
+    [SerializeField] private Slider bgmVolume;
+    [SerializeField] private Slider sfxVolume;
+
     [SerializeField] private Button toggleBtn;
-    [SerializeField] private GameObject[] objs; 
-    private bool isFrame30 = false;
+    [SerializeField] private GameObject[] objs;
+    private bool isFrame30;
 
     protected override void Awake()
     {
         base.Awake();
-        toggleBtn.onClick.AddListener(SwapImage);
+        bgmVolume.onValueChanged.AddListener(value => SoundManager.Instance.SetBgmVolume(value));
+        sfxVolume.onValueChanged.AddListener(value => SoundManager.Instance.SetSfxVolume(value));
+        toggleBtn.onClick.AddListener(SwapButtonImage);
     }
 
     private void Start()
     {
         transform.DOLocalMove(Vector3.up * 10, 1).SetAutoKill(true).SetLink(gameObject);
+        SetVolumeImage();
+        SetButtonImage();
     }
 
     public void Showpopup(int index)
@@ -30,11 +37,29 @@ public class OptionPanel : UIBase
         Hide();
     }
 
-    private void SwapImage()
+    private void SetVolumeImage()
+    {
+        bgmVolume.value = SoundManager.Instance.BgmVolume;
+        sfxVolume.value = SoundManager.Instance.GlobalSfxVolume;
+    }
+
+    private void SetButtonImage()
+    {
+        isFrame30 = Application.targetFrameRate == 30;
+        UpdateButtonImage();
+    }
+
+    private void SwapButtonImage()
     {
         isFrame30 = !isFrame30;
-        Application.targetFrameRate = Application.targetFrameRate == 60 ? 30 : 60;
-        objs[isFrame30 == true ? 1 : 0].SetActive(true);
-        objs[isFrame30 == true ? 0 : 1].SetActive(false);
+        Application.targetFrameRate = isFrame30 ? 30 : 60;
+        UpdateButtonImage();
     }
+
+    private void UpdateButtonImage()
+    {
+        objs[0].SetActive(!isFrame30); // 60
+        objs[1].SetActive(isFrame30);  // 30
+    }
+
 }
